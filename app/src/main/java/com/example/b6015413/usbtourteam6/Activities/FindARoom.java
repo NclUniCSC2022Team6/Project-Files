3d92c3880a0db0c32f7b1dc8f1fad1e91ec07dcb
Imran <imranmohammed196@googlemail.com>
2019-03-11 21:14:33 +0000

    Navigation drawer update.
    Links for the navigation drawer dont work when i swapped the code over from the version i have.
    Will update soon.
    Still not figured out the level pages 100 percent.

--- app/src/main/java/com/example/b6015413/usbtourteam6/Activities/FindARoom.java
+++ app/src/main/java/com/example/b6015413/usbtourteam6/Activities/FindARoom.java
@@ -13,10 +13,16 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -35,11 +41,12 @@ import com.example.b6015413.usbtourteam6.Table_Models.Room;
 import java.util.List;
 import java.util.Locale;
 
-public class FindARoom extends AppCompatActivity {
+public class FindARoom extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     RecyclerView recyclerView;
     List<Room> items;
     public static final int RADIUS = 300; // todo maybe this should be an int[] to vary based on the level
+    private DrawerLayout drawer;
 
     TextView findARoomTxt;
 
@@ -50,6 +57,20 @@ public class FindARoom extends AppCompatActivity {
 
         AssetManager am = this.getApplicationContext().getAssets();
         Typeface robotoLight = Typeface.createFromAsset(am, String.format(Locale.UK, "fonts/%s", "Roboto-Light.ttf"));
+
+        //Toolbar (replaces action bar)
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Nav drawer, slide in
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         findARoomTxt = findViewById(R.id.findARoomTitle);
 
@@ -98,6 +119,85 @@ public class FindARoom extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // Provides intents to correct pages
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent h= new Intent(FindARoom.this,HomePage.class);
+                startActivity(h);
+                break;
+            case R.id.nav_ground_floor:
+                Intent groundF= new Intent(FindARoom.this,LevelX.class);
+                groundF.putExtra("floor value", "Ground Floor");
+                groundF.putExtra("level", 0);
+                startActivity(groundF);
+                break;
+            case R.id.nav_first_floor:
+                Intent firstF= new Intent(FindARoom.this,LevelX.class);
+                firstF.putExtra("floor value", "First Floor");
+                firstF.putExtra("level", 1);
+                startActivity(firstF);
+                break;
+            case R.id.nav_second_floor:
+                Intent secondF= new Intent(FindARoom.this,LevelX.class);
+                secondF.putExtra("floor value", "Second Floor");
+                secondF.putExtra("level", 2);
+                startActivity(secondF);
+                break;
+            case R.id.nav_third_floor:
+                Intent thirdF= new Intent(FindARoom.this,LevelX.class);
+                thirdF.putExtra("floor value", "Third Floor");
+                thirdF.putExtra("level", 3);
+                startActivity(thirdF);
+                break;
+            case R.id.nav_fourth_floor:
+                Intent fourthF= new Intent(FindARoom.this,LevelX.class);
+                fourthF.putExtra("floor value", "Fourth Floor");
+                fourthF.putExtra("level", 4);
+                startActivity(fourthF);
+                break;
+            case R.id.nav_fifth_floor:
+                Intent fifthF= new Intent(FindARoom.this,LevelX.class);
+                fifthF.putExtra("floor value", "Fifth Floor");
+                fifthF.putExtra("level", 5);
+                startActivity(fifthF);
+                break;
+            case R.id.nav_sixth_floor:
+                Intent sixthF= new Intent(FindARoom.this,LevelX.class);
+                sixthF.putExtra("floor value", "Sixth Floor");
+                sixthF.putExtra("level", 6);
+                startActivity(sixthF);
+                break;
+            case R.id.nav_find_room:
+                Intent i= new Intent(FindARoom.this,FindARoom.class);
+                startActivity(i);
+                break;
+            case R.id.nav_building_info:
+                Intent b= new Intent(FindARoom.this,BuildingInfo.class);
+                startActivity(b);
+                break;
+            case R.id.nav_settings:
+                Intent g= new Intent(FindARoom.this,Settings.class);
+                startActivity(g);
+                break;
+        }
+        // Closes nav drawer once a new activity has been opened.
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    // Used to close/open the navigation drawer
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 
     // the drawing over an image was thanks to https://stackoverflow.com/questions/18520287/draw-a-circle-on-an-existing-image
     public void openFloorPlan(Room room) {
--- app/src/main/java/com/example/b6015413/usbtourteam6/Activities/HomePage.java
+++ app/src/main/java/com/example/b6015413/usbtourteam6/Activities/HomePage.java
@@ -3,8 +3,14 @@ package com.example.b6015413.usbtourteam6.Activities;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +21,11 @@ import com.example.b6015413.usbtourteam6.R;
 
 import java.util.Locale;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
     Button welcomeButton, groundFloor, firstFloor, secondFloor, thirdFloor,
             fourthFloor, fifthFloor, sixthFloor, findARoom;
+    private DrawerLayout drawer;
 
 
     @Override
@@ -28,6 +35,20 @@ public class HomePage extends AppCompatActivity {
 
         AssetManager am = this.getApplicationContext().getAssets();
         Typeface robotoBlack = Typeface.createFromAsset(am,String.format(Locale.UK,"fonts/%s","Roboto-Black.ttf"));
+
+        //Toolbar (replaces action bar)
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Nav drawer, slide in
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         //region set IDs
         welcomeButton = (Button)findViewById(R.id.welcomeBtn);
@@ -144,6 +165,83 @@ public class HomePage extends AppCompatActivity {
 
         //endregion
 
+    }
+
+    // Provides intents to correct pages
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent h= new Intent(HomePage.this,HomePage.class);
+                startActivity(h);
+            case R.id.nav_ground_floor:
+                Intent groundF= new Intent(HomePage.this,LevelX.class);
+                groundF.putExtra("floor value", "Ground Floor");
+                groundF.putExtra("level", 0);
+                startActivity(groundF);
+                break;
+            case R.id.nav_first_floor:
+                Intent firstF= new Intent(HomePage.this,LevelX.class);
+                firstF.putExtra("floor value", "First Floor");
+                firstF.putExtra("level", 1);
+                startActivity(firstF);
+                break;
+            case R.id.nav_second_floor:
+                Intent secondF= new Intent(HomePage.this,LevelX.class);
+                secondF.putExtra("floor value", "Second Floor");
+                secondF.putExtra("level", 2);
+                startActivity(secondF);
+                break;
+            case R.id.nav_third_floor:
+                Intent thirdF= new Intent(HomePage.this,LevelX.class);
+                thirdF.putExtra("floor value", "Third Floor");
+                thirdF.putExtra("level", 3);
+                startActivity(thirdF);
+                break;
+            case R.id.nav_fourth_floor:
+                Intent fourthF= new Intent(HomePage.this,LevelX.class);
+                fourthF.putExtra("floor value", "Fourth Floor");
+                fourthF.putExtra("level", 4);
+                startActivity(fourthF);
+                break;
+            case R.id.nav_fifth_floor:
+                Intent fifthF= new Intent(HomePage.this,LevelX.class);
+                fifthF.putExtra("floor value", "Fifth Floor");
+                fifthF.putExtra("level", 5);
+                startActivity(fifthF);
+                break;
+            case R.id.nav_sixth_floor:
+                Intent sixthF= new Intent(HomePage.this,LevelX.class);
+                sixthF.putExtra("floor value", "Sixth Floor");
+                sixthF.putExtra("level", 6);
+                startActivity(sixthF);
+                break;
+            case R.id.nav_find_room:
+                Intent i= new Intent(HomePage.this,FindARoom.class);
+                startActivity(i);
+                break;
+            case R.id.nav_building_info:
+                Intent b= new Intent(HomePage.this,BuildingInfo.class);
+                startActivity(b);
+                break;
+            case R.id.nav_settings:
+                Intent g= new Intent(HomePage.this,Settings.class);
+                startActivity(g);
+                break;
+        }
+        // Closes nav drawer once a new activity has been opened.
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    // Used to close/open the navigation drawer
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
