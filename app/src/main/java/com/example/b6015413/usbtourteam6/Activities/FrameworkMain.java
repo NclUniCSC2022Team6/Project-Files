fc9f5d18693ca89c0e48795fd06bb5336cbfb624
Imran <imranmohammed196@googlemail.com>
2019-03-16 16:07:14 +0000

    Navigation drawer is set up using fragments
    What is working: Homepage, find a room, building info, settings
    Not working: all floors
    
    TODO: Convert methods so they are friendly to fragments. Research how to get the home buttons working with inflating them. Recycler view to work within a fragment class etc.

---
+++ app/src/main/java/com/example/b6015413/usbtourteam6/Activities/FrameworkMain.java
@@ -0,0 +1,103 @@
+package com.example.b6015413.usbtourteam6.Activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.b6015413.usbtourteam6.R;
+
+import java.util.Locale;
+
+public class FrameworkMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_framework_main);
+
+        //Toolbar (replaces action bar)
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Home Page Test");
+        setSupportActionBar(toolbar);
+
+        // Nav drawer, slide in
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomePage.PlaceholderFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomePage.PlaceholderFragment()).commit();
+                break;
+            case R.id.nav_find_room:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FindARoom.PlaceholderFragment()).commit();
+                break;
+            case R.id.nav_building_info:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new BuildingInfo.PlaceholderFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Settings.PlaceholderFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search_btn:
+                startActivity(new Intent(this, SearchTutor.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
