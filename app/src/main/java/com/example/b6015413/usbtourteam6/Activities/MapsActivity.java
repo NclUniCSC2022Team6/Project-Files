96fc238f8cf86d41458c6fd9a2bff6da7bee748c
Imran <imranmohammed196@googlemail.com>
2019-03-27 17:56:58 +0000

    Quick fix for maps (was crashing on some devices)

--- app/src/main/java/com/example/b6015413/usbtourteam6/Activities/MapsActivity.java
+++ app/src/main/java/com/example/b6015413/usbtourteam6/Activities/MapsActivity.java
@@ -23,7 +23,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        MapFragment mapFragment = (MapFragment) getFragmentManager() .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         location = new LatLng(getIntent().getDoubleExtra("locationLat", 0.0),
                 getIntent().getDoubleExtra("locationLng", 0.0));
